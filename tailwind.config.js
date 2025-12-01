b2bcea99e2a075edfeb6913aecbbc3cea3df052b
JeanFi675 <duheronjp@gmail.com>
2025-12-01 17:09:24 +0100

    chore: Add HTML files to Tailwind CSS content paths.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
     content: [
         "./index.html",
         "./admin.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx,html}",
     ],
     theme: {
         extend: {
